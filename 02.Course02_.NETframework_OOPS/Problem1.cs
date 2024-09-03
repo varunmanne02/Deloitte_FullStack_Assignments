cfdd9a9ae38ca9118f8cefd5fb888e2151454cbe
varunmanne02 <vamanne@deloitte.com>
2024-09-03 18:18:04 +0530

    Add files via upload

---
+++ 02.Course02_.NETframework_OOPS/Problem1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int EmpId;
+            string EmpName;
+            string EmpJob;
+            int EmpSal;
+
+            Console.WriteLine("Enter the Employee ID");
+            EmpId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Employee Name");
+            EmpName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Employee Job Title");
+            EmpJob = Console.ReadLine();
+
+            Console.WriteLine("Enter the Salary");
+            EmpSal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Employee Id: " + EmpId);
+            Console.WriteLine();
+            Console.WriteLine("Employee Name: " + EmpName);
+            Console.WriteLine();
+            Console.WriteLine("Employee Job: " + EmpJob);
+            Console.WriteLine();
+            Console.WriteLine("Employee Salary: " + EmpSal);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02.Course02_.NETframework_OOPS/Problem2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ProdId;
+            string ProdName;
+            int UnitPrice;
+            int Quantity;
+            double discount = 0;
+            double totalAmount;
+            double finalAmount;
+            double discountAmount;
+
+            Console.WriteLine("Enter the Product ID");
+            ProdId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Product Name");
+            ProdName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Unit Price");
+            UnitPrice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Quantity");
+            Quantity = int.Parse(Console.ReadLine());
+
+            totalAmount = UnitPrice * Quantity;
+            Console.WriteLine();
+
+            if (Quantity > 10)
+            {
+                discount = 0.10;
+            }
+            if (Quantity > 30)
+            {
+                discount = 0.20;
+            }
+            if (Quantity > 50)
+            {
+                discount = 0.30;
+            }
+           
+
+            discountAmount = totalAmount * discount;
+            finalAmount = totalAmount - discountAmount; 
+
+            Console.WriteLine("Product Id: " + ProdId);
+            
+            Console.WriteLine("Product Name: " + ProdName);
+            
+            Console.WriteLine("Unit Price: " + UnitPrice);
+            
+            Console.WriteLine("Quantity: " + Quantity);
+            
+            Console.WriteLine("Total Amount: " + totalAmount);
+            
+            Console.WriteLine("Discount Amount: " + discountAmount);
+            
+            Console.WriteLine("Final Amount: " + finalAmount);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
