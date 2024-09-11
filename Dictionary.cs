a885e2bd9efe2319d3d95fbff9e40a8c23501faf
varunmanne02 <vamanne@deloitte.com>
2024-09-11 09:45:08 +0530

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+
+
+    public static void Main()
+    {
+
+        // Create a new dictionary of
+        // strings, with string keys.
+        Dictionary<string, long> myDict =
+          new Dictionary<string, long>();
+
+        // Adding key/value pairs in myDict
+        myDict.Add("Ajay", 9476455589);
+        myDict.Add("Mahesh", 9845677789);
+        myDict.Add("Krishna", 6789567823);
+        myDict.Add("Prakash", 9904458976);
+        myDict.Add("Rahul", 8907865734);
+        myDict.Add("Vishal", 9876734234);
+
+        // To get count of key/value pairs in myDict
+        Console.WriteLine("Total key/value pairs" +
+              " in myDict are : " + myDict.Count);
+
+        myDict.Remove("Krishna");
+
+        // Displaying the key/value pairs in myDict
+        Console.WriteLine("\nThe key/value pairs" +
+                           " in myDict are : ");
+
+        foreach (KeyValuePair<string, long> kvp in myDict)
+        {
+            Console.WriteLine("Key = {0}, Value = {1}",
+                              kvp.Key, kvp.Value);
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ List.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("\n Product Id: {0} \n Product Name: {1} \n Unit Price: {2} \n Quantity: {3}, Category: {4} \n", ProductId, Name, UnitPrice, Quantity, Category);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+
+            Console.WriteLine("Enter 2 Product Details\n");
+            Product product;
+            for (int i = 1; i < 3; i++)
+            {
+                Console.WriteLine($"Enter details of Product {i}");
+                product = new Product();
+                Console.WriteLine("Enter Product ID:");
+                product.ProductId = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Product Name:");
+                product.Name = Console.ReadLine();
+
+                Console.WriteLine("Enter Unit Price:");
+                product.UnitPrice = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Quantity:");
+                product.Quantity = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Product Category:");
+                product.Category = Console.ReadLine();
+
+                products.Add(product);
+
+            }
+
+            Console.WriteLine("Enter the Id of the product to search");
+            int productIDToDisplay = int.Parse(Console.ReadLine());
+            Product productToDisplay = products.Find(p => p.ProductId == productIDToDisplay);
+
+            if (productToDisplay != null)
+            {
+                Console.WriteLine("Searched Product Found.");
+            }
+            else
+            {
+                Console.WriteLine("Searched Product Not Found.");
+            }
+
+            Console.WriteLine("Enter the Id of the product to remove");
+            int productIDToRemove = int.Parse(Console.ReadLine());
+            Product productToRemove = products.Find(p => p.ProductId == productIDToRemove);
+
+            if (productToRemove != null)
+            {
+                products.Remove(productToRemove);
+                Console.WriteLine("Product removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Product not found.");
+            }
+
+            
+
+            Console.WriteLine("Details of remaining products:\n");
+
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
